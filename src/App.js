8a31f3deba4df328e5bd764104deff95c6fef418
Ugochukwu Oguejiofor <54842706+ugochukwu95@users.noreply.github.com>
2019-12-03 23:16:04 +0100

    Understnding and implementing the Url Router package

--- src/App.js
+++ src/App.js
@@ -1,28 +1,17 @@
 import React, { Component } from "react";
-// import ProductsAndSuppliers from "./ProductsAndSuppliers";
 import { Provider } from "react-redux";
 import dataStore from "./store";
 import { Selector } from "./Selector";
 import { ProductDisplay } from "./ProductDisplay";
 import { SupplierDisplay } from "./SupplierDisplay";
-import { StoreAccess } from "./store/StoreAccess";
 
 export default class App extends Component {
   render() {
-    return <div className="container-fluid">
-		<div className="row">
-			<div className="col-3">
-				<StoreAccess store={ dataStore } />
-			</div>
-			<div className="col">
-				<Provider store={ dataStore }>
-			    	<Selector>
-						<ProductDisplay name="Products" />
-						<SupplierDisplay name="Suppliers" />
-					</Selector>
-		    	</Provider>
-		    </div>
-		</div>
-	</div>
+    return <Provider store={ dataStore }>
+		<Selector>
+			<ProductDisplay name="Products" />
+			<SupplierDisplay name="Suppliers" />
+		</Selector>
+	</Provider>
   }
 }
--- src/Selector.js
+++ src/Selector.js
@@ -1,7 +1,10 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import { ProductDisplay } from "./ProductDisplay";
+import { SupplierDisplay } from "./SupplierDisplay";
 
 export class Selector extends Component {
-	constructor(props) {
+	/*constructor(props) {
 		super(props);
 		this.state = {
 			selection: React.Children.toArray(props.children)[0].props.name
@@ -11,25 +14,22 @@ export class Selector extends Component {
 	setSelection = (ev) => {
 		ev.persist();
 		this.setState({ selection: ev.target.name});
-	}
+	}*/
 
 	render() {
-		return <div className="container-fluid">
-			<div className="row">
-				<div className="col-2">
-					{ React.Children.map(this.props.children, c =>
-						<button name={ c.props.name } onClick={ this.setSelection } className={`btn btn-block m-2
-							${this.state.selection === c.props.name ? "btn-primary active": "btn-secondary"}`}>
-							{ c.props.name }
-						</button>
-					)}
-				</div>
-				<div className="col">
-					{
-						React.Children.toArray(this.props.children).filter(c => c.props.name === this.state.selection)
-					}
+		return <Router>
+			<div className="container-fluid">
+				<div className="row">
+					<div className="col-2">
+						<div><Link to="/products">Products</Link></div>
+						<div><Link to="/suppliers">Suppliers</Link></div>
+					</div>
+					<div className="col">
+						<Route path="/products" component={ ProductDisplay } />
+						<Route path="/suppliers" component={ SupplierDisplay} />
+					</div>
 				</div>
 			</div>
-		</div>
+		</Router>
 	}
 }
