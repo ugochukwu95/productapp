4528009f17a8985d1b55ae7e67b291629a206bba
Ugochukwu Oguejiofor <54842706+ugochukwu95@users.noreply.github.com>
2019-12-03 16:59:36 +0100

    Observing data store changes
    Implementing the subscribe method

--- src/store/StoreAccess.js
+++ src/store/StoreAccess.js
@@ -7,11 +7,13 @@ export class StoreAccess extends Component {
 			product: (storeState) => storeState.modelData.products[0],
 			state: (storeState) => storeState.stateData
 		}
+
+		this.state = this.selectData();
 	}
 	render() {
 		return <div className="bg-info">
 			<pre className="text-white">
-				{ JSON.stringify(this.selectData(), null, 2) }
+				{ JSON.stringify(this.state, null, 2) }
 			</pre>
 		</div>
 	}
@@ -20,5 +22,19 @@ export class StoreAccess extends Component {
 		let storeState = this.props.store.getState();
 		return Object.entries(this.selectors).map(([k, v]) => [k, v(storeState)])
 			.reduce((result, [k, v]) => ({ ...result, [k]: v}), {});
+	}
+
+	handleDataStoreChange() {
+		let newData = this.selectData();
+		Object.keys(this.selectors).filter(key => this.state[key] !== newData[key])
+			.forEach(key => this.setState({ [key]: newData[key]}));
+	}
+	
+	componentDidMount() {
+		this.unsubscriber = this.props.store.subscribe(() => this.handleDataStoreChange());
+	}
+
+	componentWillUnmount() {
+		this.unsubscriber();
 	}
 }
