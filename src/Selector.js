ed0304b6681724908bb1ba13e572783d1ee4bd98
Ugochukwu Oguejiofor <54842706+ugochukwu95@users.noreply.github.com>
2019-12-04 17:39:12 +0100

    Using redirection as a fallback route

--- src/Selector.js
+++ src/Selector.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import { BrowserRouter as Router, Link, Route, Switch, Redirect } from "react-router-dom";
 import { ProductDisplay } from "./ProductDisplay";
 import { SupplierDisplay } from "./SupplierDisplay";
 
@@ -15,18 +15,23 @@ export class Selector extends Component {
 		ev.persist();
 		this.setState({ selection: ev.target.name});
 	}*/
+	renderMessage = (msg) => <h5 className="bg-info text-white m-2 p-2">{ msg }</h5>
 
 	render() {
 		return <Router>
 			<div className="container-fluid">
 				<div className="row">
 					<div className="col-2">
+						<div><Link to="/">Default URL</Link></div>
 						<div><Link to="/products">Products</Link></div>
 						<div><Link to="/suppliers">Suppliers</Link></div>
 					</div>
 					<div className="col">
-						<Route path="/products" component={ ProductDisplay } />
-						<Route path="/suppliers" component={ SupplierDisplay} />
+						<Switch>
+							<Route path="/products" component={ ProductDisplay} />
+							<Route path="/suppliers" component={ SupplierDisplay } />
+							<Redirect to="/products" />
+						</Switch>
 					</div>
 				</div>
 			</div>
