1d951d72027ebb0d6df8a9e6ed26ecdde3fa9800
Ugochukwu Oguejiofor <54842706+ugochukwu95@users.noreply.github.com>
2019-12-18 09:05:00 +0100

    Implementing Advanced Routing Features

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import React, { Component } from "react";
 import { Provider } from "react-redux";
 import dataStore from "./store";
 import { Selector } from "./Selector";
-import { ProductDisplay } from "./ProductDisplay";
-import { SupplierDisplay } from "./SupplierDisplay";
+import { PRODUCTS, SUPPLIERS } from "./store/dataTypes";
 
 export default class App extends Component {
   render() {
     return <Provider store={ dataStore }>
 		<Selector>
-			<ProductDisplay name="Products" />
-			<SupplierDisplay name="Suppliers" />
+			<data name="Products" datatype={ PRODUCTS } />
+		<data name="Suppliers" datatype ={ SUPPLIERS } />
 		</Selector>
 	</Provider>
   }
--- src/store/EditorConnector.js
+++ src/store/EditorConnector.js
@@ -1,22 +1,35 @@
 import { connect } from "react-redux";
-import { endEditing } from "./stateActions";
-import { saveProduct, saveSupplier } from "./modelActionCreators";
+// import { endEditing } from "./stateActions";
 import { PRODUCTS, SUPPLIERS } from "./dataTypes";
+import { saveAndEndEditing } from "./multiActionCreators";
+import { withRouter } from "react-router-dom";
 
 export const EditorConnector = (dataType, presentationComponent) => {
-	const mapStateToProps = (storeData) => ({
-		editing: storeData.stateData.editing && storeData.stateData.selectedType === dataType,
-		product: (storeData.modelData[PRODUCTS].find(p => p.id === storeData.stateData.selectedId)) || {},
-		supplier:(storeData.modelData[SUPPLIERS].find(s => s.id === storeData.stateData.selectedId)) || {}
-	})
+		const mapStateToProps = (storeData, ownProps) => {
+		const mode = ownProps.match.params.mode;
+		const id = Number(ownProps.match.params.id);
+		return {
+			editing: mode === "edit" || mode === "create",
+			product: (storeData.modelData[PRODUCTS].find(p => p.id === id)) || {},
+			supplier:(storeData.modelData[SUPPLIERS].find(s => s.id === id)) || {}
+		}
+	}
 
 	const mapDispatchToProps = dispatch => ({
-		cancelCallback: () => dispatch(endEditing()),
-		saveCallback: (data) => {
-			dispatch((dataType === PRODUCTS ? saveProduct: saveSupplier)(data));
-			dispatch(endEditing());
-		}
+		// cancelCallback: () => dispatch(endEditing()),
+		saveCallback: (data) => saveAndEndEditing(data, dataType)
 	});
 
-	return connect(mapStateToProps, mapDispatchToProps)(presentationComponent);
+	const mergeProps = (dataProps, functionProps, ownProps) => {
+let routedDispatchers = {
+cancelCallback: () => ownProps.history.push(`/${dataType}`),
+saveCallback: (data) => {
+functionProps.saveCallback(data);
+ownProps.history.push(`/${dataType}`);
+}
+}
+return Object.assign({}, dataProps, routedDispatchers, ownProps);
+}
+return withRouter(connect(mapStateToProps,
+mapDispatchToProps, mergeProps)(presentationComponent));
 }
